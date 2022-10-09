4c56d0c953680c489eff131c7209a8fd76c33bbb
Zahid-Ibrahim <115426812+Zahid-Ibrahim@users.noreply.github.com>
2022-10-10 00:53:03 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,577 @@
+// console.log("***Assignment 2***");
+// var name1 = '"Hello Eric, would you like to learn some Python today?”';
+// console.log(name1);
+
+// console.log("***Assignment 3***");
+// var name2 = "Zahid ibrahim";
+// name3 = name2.toLowerCase();
+// name4 = name2.toUpperCase();
+// name5 = name2.split(" ").reduce( (s, c) => s +""+(c.charAt(0).toUpperCase() + c.slice(1) +" "), '');
+// console.log(name3);
+// console.log(name4); 
+// console.log(name5);
+
+// console.log("***Assignment 4***");
+// var name6 = 'Albert Einstein once said, “A person who never made a mistake never tried anything new.”';
+// console.log(name6);
+
+// console.log("***Assignment 5***");
+// var famous_person = "Albert Einstein once said ";
+//  var message = '“A person who never made a mistake never tried anything new.”';
+// console.log(famous_person.concat(message));
+
+// console.log("***Assignment 6***");
+// var name7 = "  Zahid\t Ibrahim  ";
+// console.log(name7);
+// console.log(name7.replace(/\s+/g, ''));
+
+// console.log("***Assignment 7***");
+// var name8 ="8 +,-,*,/";
+// console.log(name8);
+
+// console.log("***Assignment 8***");
+// console.log(6+2);
+// console.log(9-1);
+// console.log(4*2);
+// console.log(16/2);
+
+// console.log("***Assignment 9***");
+// var favourite_number =7;
+// console.log("My favourite number is: " +favourite_number);
+
+//console.log("***Assignment 10***");
+//console.log("Zahid Ibrahim");
+//console.log(Date());
+//var favourite_number =7;
+//console.log("My favourite number is: " +favourite_number);
+//This program concatenates int to string
+
+// console.log("***Assignment 11***");
+// const friends = ["Rizwan", "Talha", "Tayyab" ];
+// let fLen = friends.length;
+// for (let i = 0; i < fLen; i++) {
+//   console.log(friends[i]);
+// }
+
+// console.log("***Assignment 12***");
+// const friends = ["Rizwan", "Talha", "Tayyab" ];
+// var frd_msg = "Good Morning ";
+// let fLen = friends.length;
+// for (let i = 0; i < fLen; i++) {
+//   console.log(frd_msg+friends[i]);
+// }
+
+// console.log("***Assignment 13***");
+// var fav_transport = "Car";
+// var transport1 = "I would like to own a Corolla Car";
+// var transport2 = "They have sufficient space";
+// var transport3 = "Fuel Average is good";
+// console.log(fav_transport);
+// console.log(transport1);
+// console.log(transport2);
+// console.log(transport3);
+
+//  console.log("***Assignment 14***");
+//  const friends = ["Rizwan", "Talha", "Tayyab" ];
+//  var frd_msg = "Please come for the Dinner on 10th Oct ";
+//  let fLen = friends.length;
+//  for (let i = 0; i < fLen; i++) {
+//    console.log(friends[i] +frd_msg);
+//  }
+ 
+//  console.log("***Assignment 15***");
+//  const friends = ["Rizwan", "Talha", "Tayyab" ];
+//  var frd_msg = " Please come for the Dinner on 10th Oct ";
+//  let fLen = friends.length;
+//  for (let i = 0; i < fLen; i++) {
+//    console.log(friends[i] +frd_msg);
+// }
+// console.log(friends[1]+ " won't be able to attend the dinner");
+// delete friends[1];
+// friends[1]= "Noman ";
+// console.log(friends[1]+frd_msg);
+
+// console.log("***Assignment 16***");
+//  const friends = ["Rizwan", "Talha", "Tayyab" ];
+//  var frd_msg = " Please come for the Dinner on 10th Oct ";
+//  let fLen = friends.length;
+//  for (let i = 0; i < fLen; i++) {
+//    console.log(friends[i] +frd_msg);
+// }
+// console.log("We found a bigger table");
+// friends.unshift("Umair");
+// friends.push("Waqas");
+// friends.splice(2, 0, 'Yaqoob');
+// let fLen1 = friends.length;
+//  for (let i = 0; i < fLen1; i++) {
+//    console.log(friends[i] +frd_msg);
+// }
+
+// console.log("***Assignment 17***");
+//  const friends = ["Rizwan", "Talha", "Tayyab" ];
+//  var frd_msg = " Please come for the Dinner on 10th Oct ";
+//  let fLen = friends.length;
+//  for (let i = 0; i < fLen; i++) {
+//    console.log(friends[i] +frd_msg);
+// }
+// console.log("We found a bigger table");
+// friends.unshift("Umair");
+// friends.push("Waqas");
+// friends.splice(2, 0, 'Yaqoob');
+// let fLen1 = friends.length;
+//  for (let i = 0; i < fLen1; i++) {
+//    console.log(friends[i] +frd_msg);
+// }
+// console.log("Could not recieve the table. Only two people can sit on the table");
+// frd_newmsg=" Welcome to the Dinner"
+// let fLen2 = 2;
+//  for (let i = 0; i < fLen2; i++) {
+//    console.log(friends[i] +frd_newmsg);
+// }
+// console.log(friends[2]+" Sorry no Space");
+// console.log(friends[3]+" Sorry no Space");
+// console.log(friends[4]+" Sorry no Space");
+// console.log(friends[5]+" Sorry no Space");
+
+//  console.log("***Assignment 18***");
+//  const friends = ["NewYork","Houston", "Dubai", "Abu Dhabi","Tornoto"];
+//  console.log(friends);
+//  console.log("Alphabetical Order");
+//  const fsort = []; 
+//  fsort.push(friends);
+//  console.log(friends.sort());
+//  console.log("Original List");
+//  console.log(fsort);
+//  console.log("Reverse Order");
+//  const rsort = []; 
+//   rsort.push(fsort);
+// console.log(friends.reverse());
+// console.log("Original List");
+// console.log(rsort);
+
+//console.log("***Assignment 19***");
+ // const friends = ["Rizwan", "Talha", "Tayyab" ];
+ // console.log("Number of Guests attending the dinner is :" +friends.length);
+
+// console.log("***Assignment 20***");
+// const languages = ["English", "Chinese", "German","French" ];
+// console.log("Most Popular Languages are");
+// let fLen = languages.length;
+// for (let i = 0; i < fLen; i++) {
+//     console.log(languages[i]);
+//  }
+
+//  console.log("***Assignment 21***");
+// const languages = ["English", "Chinese", "German","French" ];
+// const cities = ["Dubai", "NewYork", "Toronto","Karachi" ];
+// const person = {
+//   languages:"English",
+//   cities:"Dubai"
+//   }
+//   console.log(person.languages.valueOf());
+//   console.log(person.cities.valueOf());
+
+// console.log("***Assignment 22***");
+// const languages = ["English", "Chinese", "German","French" ];
+// console.log("Intentional Error");
+// console.log(languages[4]);
+// console.log(languages[3]);
+
+// console.log("***Assignment 23***");
+// const cars = ["Subaru", "Corolla"];
+// for (let i = 0; i < 5; i++) {
+//   if(cars[0]==="Subaru")
+//   {
+//   console.log("True");
+//   }
+//    }
+//    for (let i = 0; i < 5; i++) {
+//     if(cars[1]==="Corolla")
+//     {
+//     console.log("False");
+//     }
+//      }
+  
+// console.log("***Assignment 24***");
+// var cars = "Subaru";
+//    if(cars.toString()==="Subaru")
+//    {
+//    console.log("True");
+//    }
+   
+//    if(cars.toString()!="Subaru")
+//      {
+//      console.log("True");
+//      }
+
+// if(cars.toLowerCase()==="subaru")
+// {
+//   console.log("Lowercase:True");
+// }    
+
+// 
+
+// console.log("***Assignment 26***");
+// var alien_color = "green";
+
+// if(alien_color.toString()==="green")
+//     {
+//     console.log("Player earned 5 points for shooting the alien.");
+//     }
+// else
+// {
+//   console.log("Player earned 10 points");
+// }
+
+// console.log("***Assignment 27***");
+// var alien_color = "yellow";
+
+// if(alien_color.toString()==="green")
+//     {
+//     console.log("Player earned 5 points.");
+//     }
+// else if(alien_color.toString()==="yellow")
+// {
+//   console.log("Player earned 10 points");
+// }
+// else
+// {
+//   console.log("Player earned 15 points");
+// }
+
+// console.log("***Assignment 28***");
+// let age = 40;
+
+// if(age<2)
+//     {
+//     console.log("Person is a baby.");
+//     }
+// else if(age>=2 && age<4)
+// {
+//   console.log("Person is a toddler.");
+// }
+// else if(age>=4 && age<13)
+// {
+//   console.log("Person is a kid");
+// }
+// else if(age>=13 && age<20)
+// {
+//   console.log("Person is a Teenager");
+// }
+// else if(age>=20 && age<65)
+// {
+//   console.log("Person is an Adult");
+// }
+// else
+// {
+//   console.log("Person is elder");
+// }
+
+// console.log("***Assignment 29***");
+// let fav_fruit = ['bananas','apples','grapes']
+
+// if(fav_fruit.includes('bananas'))
+// {
+//   console.log("You selected Bananas.");
+// }
+//  if(fav_fruit.includes('apples'))
+// {
+//   console.log("You selected Apples.");
+// }
+//  if(fav_fruit.includes('orange'))
+// {
+//   console.log("You selected Oranges.");
+// }
+//  if(fav_fruit.includes('grapes'))
+// {
+//   console.log("You selected Grapes.");
+// }
+// if(fav_fruit.includes('peach'))
+// {
+//   console.log( "You selected none");
+// }
+
+//  console.log("***Assignment 30***");
+//  let list_friends = ['rizwan','talha','tayyab','zahid']
+//  let fLen = list_friends.length;
+//    for (let i = 0; i < fLen; i++) 
+//    {
+//     if(list_friends.includes('admin'))
+//  {
+//    console.log("Hello admin, would you like to see a status report?");
+//  }
+//  else
+//  {
+//    console.log("Hello "+list_friends[i]+" Thank you for logging in again.");
+//  }
+// }
+
+//  console.log("***Assignment 31***");
+//  let list_friends = []
+
+//  if(list_friends.includes('admin'))
+//  {
+//    console.log("Hello admin, would you like to see a status report?");
+//  }
+//  else
+//  {
+//    console.log("We need to find some users!");
+//  }
+
+  //  console.log("***Assignment 32***");
+  //  let current_users = ['rizwan','talha','tayyab','zahid']
+  //  let new_users = ['noman','talha','umair','zahid']
+
+  //  let fLen = new_users.length;
+  //  for (let i = 0; i < fLen; i++)
+  //  {
+  //    if(new_users[i].toString()===current_users.toString())
+
+  //    {
+  //      console.log("Sorry "+new_users[i]+" Name has already taken");
+  //    }
+  //    else
+  //    {
+  //     console.log("Great "+new_users[i]+" is still available.");
+  //    }
+  //   }
+  
+    /* console.log("***Assignment 33***");
+    let ordinal_numbers = ['1','2','3','4','5','6','7','8','9']
+
+    let fLen = ordinal_numbers.length;
+    for (let i = 0; i < fLen; i++)
+    {
+  if(ordinal_numbers[i]==='1')
+{
+  console.log("1st");
+  }
+  else if(ordinal_numbers[i]==='2')
+  {
+    console.log("2nd");
+  }
+  else if(ordinal_numbers[i]==='3')
+  {
+    console.log("3rd");
+  }
+  else if(ordinal_numbers[i]==='4')
+  {
+    console.log("4th");
+  }
+  else if(ordinal_numbers[i]==='5')
+  {
+    console.log("5th");
+  }
+  else if(ordinal_numbers[i]==='6')
+  {
+    console.log("6th");
+  }
+  else if(ordinal_numbers[i]==='7')
+  {
+    console.log("7th");
+  }
+  else if(ordinal_numbers[i]==='8')
+  {
+    console.log("8th");
+  }
+  else if(ordinal_numbers[i]==='9')
+  {
+    console.log("9th");
+  }
+  else
+  {
+    console.log("No NUmber");
+  }
+} */
+
+/* console.log("***Assignment 34***");
+let fav_pizza = ['Tikka','Fajita','Cheese']
+
+let fLen = fav_pizza.length;
+let gLen = fav_pizza.length;
+
+for (let i = 0; i < fLen; i++)
+{ 
+  console.log(fav_pizza[i]);
+ }
+
+ for (let j = 0; j < gLen; j++)
+{ 
+  console.log("I like "+fav_pizza[j]+ " pizza");
+ }
+console.log("I really love Pizza.")  */
+ 
+/* console.log("***Assignment 35***");
+let animals = ['Dog','Cat','Rabbit']
+
+let fLen = animals.length;
+let gLen = animals.length;
+let kLen = animals.length;
+
+for (let i = 0; i < fLen; i++)
+{ 
+  console.log(animals[i]);
+ }
+
+ for (let j = 0; j < gLen; j++)
+{ 
+  console.log(animals[j]+ " would make a good pet");
+ }
+
+ for (let k = 0; k < gLen; k++)
+{ 
+  console.log(animals[k]+ " have four legs");
+ } */
+
+/* console.log("***Assignment 36***");
+//let size_shirt = ['small','medium','large']
+function make_shirt(size,msg)
+{
+  console.log(size+" shirt");
+  console.log(msg);
+}
+console.log(make_shirt('small','Thank you for ordering')); */
+
+/* console.log("***Assignment 37***");
+let size_shirt = []
+let message = []
+
+console.log(make_shirt('large','i love javascript'));
+console.log(make_shirt('medium','i love javascript'));
+
+function make_shirt(size_shirt,message)
+{
+  
+  console.log(size_shirt);
+  console.log(message); 
+
+  } */
+
+/* console.log("***Assignment 38***");
+let city_name = []
+let country = []
+
+describe_city('Karachi','Pakistan');
+describe_city('Dubai','UAE');
+describe_city('New York','USA');
+
+function describe_city(city_name,country)
+{
+  console.log(city_name+" is in "+country);
+}
+ */
+
+/* console.log("***Assignment 39***");
+let city_name = []
+let country = []
+
+describe_city('Karachi','Pakistan');
+describe_city('Dubai','UAE');
+describe_city('New York','USA');
+
+function describe_city(city_name,country)
+{
+  console.log('"'+city_name+','+country+'"');
+} */
+
+/* console.log("***Assignment 40***");
+var Albums = [
+  {artist_name:'Ali Azmat', album_title: 'Junoon'},
+  {artist_name:'Atif Aslam', album_title: 'Doorie'},
+  {artist_name:'Strings', album_title: 'Jago'},
+];
+console.log(Object.values(Albums)[0]);
+console.log(Object.values(Albums)[1]);
+console.log(Object.values(Albums)[2]); */
+  
+/* console.log("***Assignment 41***");
+var magician_name = ["David","Lance","Mark"]
+show_magician(magician_name);
+function show_magician(magician_name)
+{
+let fLen = magician_name.length;
+for (let i = 0; i < fLen; i++)
+{ 
+  console.log(magician_name[i]);
+ }
+} */
+
+/* console.log("***Assignment 42***");
+var magician_name = ["David","Lance","Mark"]
+show_magician(magician_name);
+make_great(magician_name);
+
+function show_magician(magician_name)
+{
+let fLen = magician_name.length;
+for (let i = 0; i < fLen; i++)
+{ 
+  console.log(magician_name[i]);
+ }
+}
+ function make_great(magician_name)
+{
+let fLen = magician_name.length;
+for (let i = 0; i < fLen; i++)
+{ 
+  console.log(magician_name[i]+" The Great");
+ }
+}
+  */
+
+/* console.log("***Assignment 43***");
+var magician_name = ["David","Lance","Mark"]
+show_magician(magician_name);
+make_great(magician_name);
+show_magician(magician_name);
+
+function show_magician(magician_name)
+{
+let fLen = magician_name.length;
+for (let i = 0; i < fLen; i++)
+{ 
+  console.log(magician_name[i]);
+ }
+}
+ function make_great(magician_name)
+{
+let fLen = magician_name.length;
+for (let i = 0; i < fLen; i++)
+{ 
+  console.log(magician_name[i]+" The Great");
+ }
+}
+ */
+
+/* console.log("***Assignment 44***");
+var  items= ["Lettuce","Cucumber","Sauce"]
+var sandwich="chicken sandwich"
+make_sandwich(sandwich,items);
+function make_sandwich(sandwich,items)
+{
+  console.log(sandwich);
+let fLen = items.length;
+for (let i = 0; i < fLen; i++)
+{ 
+  console.log(items[i]);
+ }
+} */
+
+/* console.log("***Assignment 45***");
+var manufacturer_name="Honda"
+var model_name="City"
+print_cars(manufacturer_name,model_name)
+function print_cars(manufacturer_name,model_name)
+{
+console.log("Manufacturer Name: "+manufacturer_name);
+console.log("Model Name: "+model_name);
+
+var car_features = [
+  {color:'white', optional: 'power windows'},
+  {color:'blue', optional: 'power_steering'},
+  {color:'silver', optional: 'turbo'},
+];
+console.log(Object.values(car_features)[0]);
+console.log(Object.values(car_features)[1]);
+console.log(Object.values(car_features)[2]); 
+} */
